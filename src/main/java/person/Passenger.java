6167e17dfd6a371cbdd60f3bdd0037ab0a6ced9d
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-10-01 14:26:35 +0100

    Passenger tests passing

--- src/main/java/person/Passenger.java
+++ src/main/java/person/Passenger.java
@@ -1,4 +1,19 @@
 package person;
 
-public class Passenger {
+public class Passenger extends Person {
+
+    private int numberOfBags;
+
+    public Passenger(String name, int numberOfBags) {
+        super(name);
+        this.numberOfBags = numberOfBags;
+    }
+
+    public int getNumberOfBags() {
+        return numberOfBags;
+    }
+
+    public void setNumberOfBags(int numberOfBags) {
+        this.numberOfBags = numberOfBags;
+    }
 }
--- src/test/java/person/PassengerTest.java
+++ src/test/java/person/PassengerTest.java
@@ -1,4 +1,27 @@
 package person;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    Passenger passenger;
+
+    @Before
+    public void before() {
+        passenger = new Passenger("Nancy Mitford", 3);
+    }
+
+    @Test
+    public void hasNumberOfBags() {
+        assertEquals(3, passenger.getNumberOfBags());
+    }
+
+    @Test
+    public void canChangeNumberOfBags() {
+        passenger.setNumberOfBags(5);
+        assertEquals(5, passenger.getNumberOfBags());
+    }
 }
